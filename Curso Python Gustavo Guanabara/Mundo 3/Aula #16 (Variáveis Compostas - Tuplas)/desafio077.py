a41ca543055923b66cc18d394b0a6c8ac9a6bac0
GohanLearn <darkkurama41@gmail.com>
2026-01-13 14:48:14 -0300

    correção

--- Curso Python Gustavo Guanabara/Mundo 3/Aula #16 (Variáveis Compostas - Tuplas)/desafio077.py
+++ Curso Python Gustavo Guanabara/Mundo 3/Aula #16 (Variáveis Compostas - Tuplas)/desafio077.py
@@ -1,9 +1,7 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são suas vogais.
 palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
-v = 'AEIOUaeiou'
 for i in palavras:
-    print(f'Na palavra {i.upper()} temos ', end = '')
+    print(f'\nNa palavra {i.upper()} temos ', end = '')
     for n in i:
-        if n in v:
+        if n in 'AEIOUaeiou':
          print(n, end = ' ')
-    print('\n')
