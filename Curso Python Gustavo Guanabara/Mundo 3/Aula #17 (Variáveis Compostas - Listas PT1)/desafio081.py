321c61ca8411b7275cfdfffba931ac65ff05e7c6
GohanLearn <darkkurama41@gmail.com>
2026-01-15 13:52:55 -0300

    Desafio 81 feito

--- Curso Python Gustavo Guanabara/Mundo 3/Aula #17 (Variáveis Compostas - Listas PT1)/desafio081.py
+++ Curso Python Gustavo Guanabara/Mundo 3/Aula #17 (Variáveis Compostas - Listas PT1)/desafio081.py
@@ -5,3 +5,15 @@
 # C) Se o valor 5 foi digitado e está ou não na lista.
 
 # programa:
+numeros = []
+escolha = 'S'
+while escolha in 'Ss':
+    numeros.append(int(input('Digite um número: ')))
+    escolha = str(input('Quer continuar? [S/N] ')).strip()[0]
+print(f'Ao todo, foram digitados {len(numeros)} elementos.')
+numeros.sort(reverse=True)
+print(f'A lista ordenada em ordem decrescente: {numeros}')
+if 5 in numeros:
+    print(f'O número 5 está na posição {numeros.index(5)} e está na lista!')
+else:
+    print('O número 5 não está na lista.')
