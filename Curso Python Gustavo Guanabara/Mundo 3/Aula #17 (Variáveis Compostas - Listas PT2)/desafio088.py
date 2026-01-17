7ddca50f48720766e6f8aa1b8341a9d662ec8e1a
GohanLearn <darkkurama41@gmail.com>
2026-01-17 19:41:44 -0300

    Desafio 088 Resolvido

--- Curso Python Gustavo Guanabara/Mundo 3/Aula #17 (Variáveis Compostas - Listas PT2)/desafio088.py
+++ Curso Python Gustavo Guanabara/Mundo 3/Aula #17 (Variáveis Compostas - Listas PT2)/desafio088.py
@@ -15,9 +15,15 @@ print('-'*40)
 jogos = int(input('Quantos jogos você quer que eu sorteie? '))
 print(f'-=-=-=- SORTEANDO {jogos} JOGOS -=-=-=-')
 for s in range(0, jogos):
+    for p in range(0, 6):
+        palpite = randint(1, 60)
+        while palpite in palpites:
+            palpite = randint(1, 60)
+        palpites.append(palpite)
+    palpites.sort()
+    jogadas.append(palpites[:])
+    palpites.clear()
+for j in jogadas:
     sleep(1)
-    palpites.append(randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60))
-    for a in palpites:
-        if palpites.count(a) > 1:
-            palpites.insert(a, randint(1, 60))
-            palpites.pop(a)
+    print(f'Jogo {jogadas.index(j)+1}: {j}')
+print('-=-=-=-=-=- BOA SORTE! -=-=-=-=-=-')
