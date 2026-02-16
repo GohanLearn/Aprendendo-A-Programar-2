f03f7b6ceefcd35b7dceb1589c47c95eb7e3d60b
GohanLearn <darkkurama41@gmail.com>
2026-02-16 15:44:58 -0300

    Correção Desafios 107-112

--- Curso Python Gustavo Guanabara/Mundo 3/Aula #22 (Módulos e Pacotes)/ex108/moeda.py
+++ Curso Python Gustavo Guanabara/Mundo 3/Aula #22 (Módulos e Pacotes)/ex108/moeda.py
@@ -1,18 +1,18 @@
-def moeda(val):
+def moeda(val=0):
     return f'R${val:.2f}'.replace('.', ',')
 
 
-def aumentar(val, percent):
+def aumentar(val=0, percent=0):
     return val + ((val / 100) * percent)
 
 
-def diminuir(val, percent):
+def diminuir(val=0, percent=0):
     return val - ((val / 100) * percent)
 
 
-def dobro(val):
+def dobro(val=0):
     return val * 2
 
 
-def metade(val):
+def metade(val=0):
     return val / 2
--- Curso Python Gustavo Guanabara/Mundo 3/Aula #22 (Módulos e Pacotes)/ex112/utilidadesCeV/dado/__init__.py
+++ Curso Python Gustavo Guanabara/Mundo 3/Aula #22 (Módulos e Pacotes)/ex112/utilidadesCeV/dado/__init__.py
@@ -1,11 +1,9 @@
 def leiaDinheiro(dados):
-    while True:
-        valor = str(input(f'{dados}'))
-        valor = valor.replace(',', '.')
-        if valor.isnumeric() or valor.isdecimal():
-            valor = float(valor)
-            return valor
+    válido = False
+    while not válido:
+        valor = str(input(f'{dados}')).replace(',', '.').strip()
+        if valor.isalpha() or valor == '':
+            print(f'\033[1;31mERRO: \"{valor}\" é um preço inválido!\033[m')
         else:
-            while not valor.isnumeric() or valor.isdecimal():
-                print(f'\033[1;31mERRO: "{valor}" é um preço inválido!\033[m')
-                valor = str(input(f'{dados}'))
+            válido = True
+    return float(valor)
